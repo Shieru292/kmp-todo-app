5e447532223325fd3afba5cf1b843bf6fc0e8f40
Shieru292 <dev@shieru.net>
2026-02-25 05:44:23 +0900

    Remove unused imports and dependencies from Application.kt and libs.versions.toml

--- server/src/main/kotlin/net/shieru/my_project_1/Application.kt
+++ server/src/main/kotlin/net/shieru/my_project_1/Application.kt
@@ -15,8 +15,6 @@ import io.ktor.utils.io.*
 import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.jdbc.Database
 import org.jetbrains.exposed.v1.jdbc.SchemaUtils
-import org.jetbrains.exposed.v1.jdbc.insert
-import org.jetbrains.exposed.v1.jdbc.selectAll
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 
 fun main() {
